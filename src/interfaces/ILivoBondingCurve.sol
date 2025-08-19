f97eeb02ee93cec8c362363e2f60523377e8d722
jacopod <jacolansac@gmail.com>
2025-08-19 23:12:34 +0200

    updated bonding curve interface

--- src/interfaces/ILivoBondingCurve.sol
+++ src/interfaces/ILivoBondingCurve.sol
@@ -3,14 +3,26 @@ pragma solidity 0.8.28;
 
 interface ILivoBondingCurve {
     /// @notice How many tokens can be purchased with a given amount of ETH
-    /// @param circulatingSupply Tokens already sold and in circulation
-    /// @param ethAmount Amount of ETH to spend
-    /// @return Number of tokens that can be purchased
-    function getTokensForEth(uint256 circulatingSupply, uint256 ethAmount) external view returns (uint256);
+    function ethToTokens_onBuy(uint256 circulatingSupply, uint256 ethReserves, uint256 ethAmount)
+        external
+        view
+        returns (uint256 tokensReceived);
 
-    /// @notice How much ETH is required to purchase a given amount of tokens
-    /// @param circulatingSupply Tokens already sold and in circulation
-    /// @param tokenAmount Amount of tokens to purchase
-    /// @return Amount of ETH required to purchase the tokens
-    function getEthForTokens(uint256 circulatingSupply, uint256 tokenAmount) external view returns (uint256);
+    /// @notice how many tokens have to be sold to receive amount of ETH
+    function ethToTokens_onSell(uint256 circulatingSupply, uint256 ethReserves, uint256 ethAmount)
+        external
+        view
+        returns (uint256 tokensRequired);
+
+    /// @notice how much ETH is required to buy a given amount of tokens
+    function tokensToEth_onBuy(uint256 circulatingSupply, uint256 ethReserves, uint256 tokenAmount)
+        external
+        view
+        returns (uint256 ethRequired);
+
+    /// @notice how much ETH will be received when selling a given amount of tokens
+    function tokensToEth_onSell(uint256 circulatingSupply, uint256 ethReserves, uint256 tokenAmount)
+        external
+        view
+        returns (uint256 ethReceived);
 }
