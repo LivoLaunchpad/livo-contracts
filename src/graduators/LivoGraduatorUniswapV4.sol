c7dc0499871e044326150a94e1bebbf52d9ed6ca
jacopod <jacolansac@gmail.com>
2025-10-03 00:03:07 +0200

    feat: univ4 graduator compilinig. Need to adjust liquidity numbers still

--- src/graduators/LivoGraduatorUniswapV4.sol
+++ src/graduators/LivoGraduatorUniswapV4.sol
@@ -9,18 +9,17 @@ import {SafeERC20} from "lib/openzeppelin-contracts/contracts/token/ERC20/utils/
 // import {IUniswapV2Factory} from "src/interfaces/IUniswapV2Factory.sol";
 // import {IUniswapV2Pair} from "src/interfaces/IUniswapV2Pair.sol";
 
-
 // import {ERC20} from "lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 // import {IERC20} from "lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 // // import {BasicHooks} from "src/BasicHooks.sol";
 
-// import {IHooks} from "lib/v4-core/src/interfaces/IHooks.sol";
-// import {PoolKey} from "lib/v4-core/src/types/PoolKey.sol";
-// import {IPoolManager} from "lib/v4-core/src/interfaces/IPoolManager.sol";
-// import {CurrencyLibrary, Currency} from "lib/v4-core/src/types/Currency.sol";
+import {PoolKey} from "lib/v4-core/src/types/PoolKey.sol";
+import {IPoolManager} from "lib/v4-core/src/interfaces/IPoolManager.sol";
+import {CurrencyLibrary, Currency} from "lib/v4-core/src/types/Currency.sol";
+import {IHooks} from "lib/v4-core/src/interfaces/IHooks.sol";
 
-// import {Actions} from "lib/v4-periphery/src/libraries/Actions.sol";
-// import {IPositionManager} from "lib/v4-periphery/src/interfaces/IPositionManager.sol";
+import {Actions} from "lib/v4-periphery/src/libraries/Actions.sol";
+import {IPositionManager} from "lib/v4-periphery/src/interfaces/IPositionManager.sol";
 // import {PositionInfo} from "lib/v4-periphery/src/libraries/PositionInfoLibrary.sol";
 // import {IAllowanceTransfer} from "lib/v4-periphery/lib/permit2/src/interfaces/IAllowanceTransfer.sol";
 // import {StateLibrary} from "lib/v4-core/src/libraries/StateLibrary.sol";
@@ -28,8 +27,7 @@ import {SafeERC20} from "lib/openzeppelin-contracts/contracts/token/ERC20/utils/
 // import {IPermit2} from "lib/v4-periphery/lib/permit2/src/interfaces/IPermit2.sol";
 // import {IV4Router} from "lib/v4-periphery/src/interfaces/IV4Router.sol";
 // import {Commands} from "src/dependencies/Univ4UniversalRouterCommands.sol";
-// import {LiquidityAmounts} from "lib/v4-periphery/src/libraries/LiquidityAmounts.sol";
-
+import {LiquidityAmounts} from "lib/v4-periphery/src/libraries/LiquidityAmounts.sol";
 
 contract LivoGraduatorUniswapV4 is ILivoGraduator {
     using SafeERC20 for ILivoToken;
@@ -39,14 +37,14 @@ contract LivoGraduatorUniswapV4 is ILivoGraduator {
     // /// @notice Uniswap router and factory addresses
     // IUniswapV2Router internal immutable UNISWAP_ROUTER;
     // IUniswapV2Factory internal immutable UNISWAP_FACTORY;
-    
+
     address internal immutable WETH;
 
-    // todo make these conditional to the chainId
+    // todo make these addresses conditional to the chainId
     IPoolManager public immutable poolManager = IPoolManager(0x000000000004444c5dc75cB358380D2e3dE08A90);
     IPositionManager public immutable posm = IPositionManager(0xbD216513d74C8cf14cf4747E6AaA6420FF64ee9e);
-    IUniversalRouter public immutable universalRouter = IUniversalRouter(0x66a9893cC07D91D95644AEDD05D03f95e1dBA8Af);
-    IPermit2 public immutable permit2 = IPermit2(0x000000000022D473030F116dDEE9F6B43aC78BA3);
+    // IUniversalRouter public immutable universalRouter = IUniversalRouter(0x66a9893cC07D91D95644AEDD05D03f95e1dBA8Af);
+    // IPermit2 public immutable permit2 = IPermit2(0x000000000022D473030F116dDEE9F6B43aC78BA3); // review if this is needed
 
     // fees in pips, i.e. 1e6 = 100%, so 10000 = 1%
     // todo make hook to distribute lp fees to treasury & token creator
@@ -55,12 +53,32 @@ contract LivoGraduatorUniswapV4 is ILivoGraduator {
     // tick spacing used to be 200 for volatile pairs in univ3. (60 for 0.3% fee tier)
     int24 constant tickSpacing = 200;
 
-    constructor(address _uniswapRouter, address _launchpad) {
-        LIVO_LAUNCHPAD = _launchpad;
-        UNISWAP_ROUTER = IUniswapV2Router(_uniswapRouter);
+    //////////////////////////// price setpoint ///////////////////////////////
 
-        WETH = UNISWAP_ROUTER.WETH();
-        UNISWAP_FACTORY = IUniswapV2Factory(UNISWAP_ROUTER.factory());
+    // `sqrtPriceX96` is denominated as sqrt(amountToken1/amountToken0) * 2^96
+
+    // starting price: 0.000000003 ETH/token, 333333334 tokens/ETH ->
+    // this correspond to a upper tick of 196256
+    // to align with 200 tick spacing, and include the starting price, we round up to 196400
+    int24 constant tickUpper = 196400;
+    uint160 constant sqrtPriceBX96 = 1456928274337359229878378703093760; // upper tick [token/ETH](min token price as ETH/token) --> starting price
+
+    // for the lower tick, we consider the current market cap of BTC as a the max price with liquidity (2305 billion USD)
+    // with a token supply of 1 billion, this corresponds to 2305 USD/token
+    // the price is denominated in ETH. If ETH price is 2305 USD/ETH, the token price would be 1:1.
+    // to be conservative, we consider the scenario in which ETH goes down significantly: ETH price (2.3$ / ETH).
+    // So the max price at which we will deploy liquidity will be at 1000 ETH/token
+    // The upper ticket for that price would be 69081, but to align it with -196200, we round it down to 69000, which is still pretty conservative
+    int24 constant tickLower = -69000;
+    uint160 constant sqrtPriceAX96 = 2515582309682650804192804864; // lower tick [token/ETH](max expected price)
+
+    // this starting price is roughly 338156060 tokens for 1 wei, i.e. 0.000000003 ETH/token
+    uint160 constant startingPriceX96 = sqrtPriceBX96 - 1; // just below the upper tick price. In range, but minimal eth required to mint the position.
+
+    constructor(address _launchpad, address _poolManager, address _positionManager) {
+        LIVO_LAUNCHPAD = _launchpad;
+        poolManager = IPoolManager(_poolManager);
+        posm = IPositionManager(_positionManager);
     }
 
     modifier onlyLaunchpad() {
@@ -68,9 +86,17 @@ contract LivoGraduatorUniswapV4 is ILivoGraduator {
         _;
     }
 
-    function initializePair(address tokenAddress) external override onlyLaunchpad returns (address pair) {
-        pair = UNISWAP_FACTORY.createPair(tokenAddress, WETH);
-        emit PairInitialized(tokenAddress, pair);
+    function initializePair(address tokenAddress) external override onlyLaunchpad returns (address) {
+        PoolKey memory pool = _getPoolKey(tokenAddress);
+
+        // this sets the price even if there is no liquidity yet
+        poolManager.initialize(pool, startingPriceX96);
+
+        // in univ4, there is not a pair address.
+        // We return the address of the pool manager, to prevent liquidity deposits until graduation
+        emit PairInitialized(tokenAddress, address(poolManager));
+
+        return address(poolManager);
     }
 
     function graduateToken(address tokenAddress) external payable override onlyLaunchpad {
@@ -83,113 +109,71 @@ contract LivoGraduatorUniswapV4 is ILivoGraduator {
         require(tokenBalance > 0, NoTokensToGraduate());
         require(ethValue > 0, NoETHToGraduate());
 
-        // the pair should have been pre-created at token launch.
-        // If not, pair==address(0) and graduation will revert
-        address pair = UNISWAP_FACTORY.getPair(tokenAddress, WETH);
-
         // this opens the gate of transferring tokens to the uniswap pair
         token.markGraduated();
 
-        // Approve the router to handle the tokens for liquidity addition
-        token.safeIncreaseAllowance(address(UNISWAP_ROUTER), tokenBalance);
+        // uniswap v4 liquidity position creation
+        // question where should we put the nft?
+        _mintLiquidity(tokenAddress, ethValue, tokenBalance);
 
-        // syncs and reads the actual reserves in the pair (in case there is unsynced ETH)
-        uint256 ethReserve = _getUpdatedEthReserves(pair, tokenAddress);
-
-        uint256 amountToken;
-        uint256 amountEth;
-        uint256 liquidity;
-        // We ensure that the token reserve is zero by forbidding transfers to the pair pre-graduation
-        // Therefore, here we only need to check if there is ETH in the pair
-        if (ethReserve == 0) {
-            (amountToken, amountEth, liquidity) = _naiveLiquidityAddition(tokenAddress, tokenBalance, ethValue);
-        } else {
-            // This path would almost never be executed. But we need to protect against attacks
-            // trying to DOS the graduation by sending ETH to the pair pre-graduation
-            (amountToken, amountEth, liquidity) =
-                _addLiquidityWithPriceMatching(tokenAddress, ethReserve, tokenBalance, ethValue, pair);
-        }
-
-        // handle any remaining balance in this contract
-        _cleanup(tokenAddress);
-
-        emit TokenGraduated(tokenAddress, pair, amountToken, amountEth, liquidity);
+        // emit TokenGraduated(tokenAddress, pair, tokenBalance, amountEth, liquidity);// todo
     }
 
-    /// @notice Reads the actual reserves after syncing, and returns them in the order of (token, eth)
-    function _getUpdatedEthReserves(address pair, address tokenAddress) internal returns (uint256 ethReserve) {
-        IUniswapV2Pair pairContract = IUniswapV2Pair(pair);
-
-        // in case there is unsynced ETH
-        pairContract.sync();
-
-        (uint112 reserve0, uint112 reserve1,) = pairContract.getReserves();
-
-        // Determine which reserve corresponds to which token
-        address token0 = pairContract.token0();
-
-        if (token0 == tokenAddress) {
-            ethReserve = reserve1;
-        } else {
-            ethReserve = reserve0;
-        }
+    function _getPoolKey(address tokenAddress) internal view returns (PoolKey memory) {
+        return
+            PoolKey({
+                currency0: Currency.wrap(address(0)), // native ETH
+                currency1: Currency.wrap(address(tokenAddress)),
+                fee: lpFee,
+                tickSpacing: tickSpacing,
+                hooks: IHooks(address(0)) // no hooks ? // todo build necessary hooks if relevant
+            });
     }
 
-    function _addLiquidityWithPriceMatching(
-        address tokenAddress,
-        uint256 ethReserve,
-        uint256 tokenBalance,
-        uint256 ethValue,
-        address pair
-    ) internal returns (uint256 amountToken, uint256 amountEth, uint256 liquidity) {
-        // Calculate tokens needed to match existing price
-        // review this calculation
-        uint256 tokensToTransfer = (tokenBalance * ethReserve) / (ethValue + ethReserve);
+    function _mintLiquidity(address tokenAddress, uint256 ethValue, uint256 tokenAmount) internal {
 
-        // if there was eth in the contract, then it is guaranteed that tokensToTransfer > 0
-        if (tokensToTransfer < tokenBalance) {
-            // Transfer calculated tokens directly to pair and sync
-            ILivoToken(tokenAddress).safeTransfer(pair, tokensToTransfer);
-            IUniswapV2Pair(pair).sync();
+        address excessEthRecipient = ILivoLaunchpad(LIVO_LAUNCHPAD).treasury();
 
-            // Add remaining tokens and ETH as liquidity
-            uint256 remainingTokens = tokenBalance - tokensToTransfer;
-            (amountToken, amountEth, liquidity) = UNISWAP_ROUTER.addLiquidityETH{value: ethValue}(
-                tokenAddress, remainingTokens, 0, 0, DEAD_ADDRESS, block.timestamp + 3600
-            );
-            // the tokens sent as sync also count as liquidity added ofc
-            amountToken += tokensToTransfer;
-        } else {
-            // Fallback: add all as liquidity
-            // This would only happen if some weirdo sent enough ETH so that the remaining tokens supply cannot match the price
-            // Although highly unlikely, it is important to protect against this scenario
-            (amountToken, amountEth, liquidity) = _naiveLiquidityAddition(tokenAddress, tokenBalance, ethValue);
-        }
-    }
+        PoolKey memory pool = _getPoolKey(tokenAddress);
 
-    /// @dev This blindly adds the liquidity, accepting any LPs, so accepting whatever price ratio is in the pair already
-    function _naiveLiquidityAddition(address tokenAddress, uint256 tokenBalance, uint256 ethValue)
-        internal
-        returns (uint256 amountToken, uint256 amountEth, uint256 liquidity)
-    {
-        (amountToken, amountEth, liquidity) = UNISWAP_ROUTER.addLiquidityETH{value: ethValue}(
-            tokenAddress, tokenBalance, 0, 0, DEAD_ADDRESS, block.timestamp + 3600
+        // calculate liquidity based on the startingPrice and the range
+        // calculate the liquidity range, assuming we just hit graduation exactly // todo
+        uint128 liquidity = LiquidityAmounts.getLiquidityForAmounts(
+            uint160(startingPriceX96), // current pool price --> presumably the starting price which cannot be modified until graduation
+            uint160(sqrtPriceAX96), // lower tick price
+            uint160(sqrtPriceBX96), // upper tick price
+            ethValue, // desired amount0
+            tokenAmount // desired amount1
         );
+       
+        // uint128 liquidity = 54380365000000000000000; // 1e27, somewhat arbitrary, but should be sufficient to move the price
+        // console.log("liquidity calculated:", liquidity);
+
+        // Actions for ETH liquidity positions
+        // 1. Mint position
+        // 2. Settle pair (send ETH and tokens)
+        // 3. Sweep any remaining native ETH back to the treasury (only required with native eth positions)
+        bytes memory actions =
+            abi.encodePacked(uint8(Actions.MINT_POSITION), uint8(Actions.SETTLE_PAIR), uint8(Actions.SWEEP));
+        bytes[] memory params = new bytes[](3);     
+
+        // parameters for MINT_POSITION action
+        // review if this contract should be the receiver of the position NFT
+        params[0] = abi.encode(pool, tickLower, tickUpper, liquidity, ethValue, tokenAmount, address(this), "");
+
+        // parameters for SETTLE_PAIR action
+        params[1] = abi.encode(pool.currency0, pool.currency1);
+
+        // parameters for SWEEP action
+        params[2] = abi.encode(pool.currency0, excessEthRecipient); // sweep all remaining native ETH to recipient
+
+        // the actual call to the position manager to mint the liquidity position
+        // deadline = block.timestamp (no effective deadline)
+        IPositionManager(posm).modifyLiquidities{value: ethValue}(abi.encode(actions, params), block.timestamp);
     }
 
-    function _cleanup(address tokenAddress) internal {
-        uint256 remainingTokenBalance = ILivoToken(tokenAddress).balanceOf(address(this));
-        // burn any remaining tokens
-        if (remainingTokenBalance > 0) {
-            ILivoToken(tokenAddress).safeTransfer(DEAD_ADDRESS, remainingTokenBalance);
-        }
-
-        // send any remaining ETH to the owner (launchpad)
-        uint256 remainingEth = address(this).balance;
-        if (remainingEth > 0) {
-            address livoTreasury = ILivoLaunchpad(LIVO_LAUNCHPAD).treasury();
-            (bool success,) = livoTreasury.call{value: remainingEth}("");
-            require(success, "ETH transfer to treasury failed");
-        }
+    /// @notice Any account can claim these fees, which are sent to token creators (tokens) and to livo treasury (eth)
+    function claimFees(address tokenAddress) external {
+        // todo 
     }
 }
