be1d8beabbc579ad7e4a0cdeeceaca617cf0addc
jacopod <jacolansac@gmail.com>
2026-02-15 08:50:06 +0100

    test: extra simple test for clarity

--- test/graduators/graduation.t.sol
+++ test/graduators/graduation.t.sol
@@ -59,6 +59,16 @@ abstract contract ProtocolAgnosticGraduationTests is LaunchpadBaseTests {
         launchpad.buyTokensWithExactEth{value: 1 ether}(testToken, 0, DEADLINE);
     }
 
+    /// @notice creator gets the CREATOR_GRADUATION_COMPENSATION at graduation
+    function test_creatorGetsGraduationCompensation() public createTestToken {
+        uint256 balanceBefore = creator.balance;
+        
+        _graduateToken();
+
+        uint256 creatorBalanceAfter = creator.balance;
+        assertEq(creatorBalanceAfter, balanceBefore + CREATOR_GRADUATION_COMPENSATION, "Creator should receive graduation compensation");
+    }
+
     /// @notice Test that tokens cannot be sold to the launchpad after graduation
     function test_tokensCannotBeSoldToLaunchpadAfterGraduation() public createTestToken {
         _graduateToken();
