02fcf0da658f150ebba3b402e9da715e170f84cb
jacopod <jacolansac@gmail.com>
2026-03-14 23:35:13 +0100

    feat: inverse quote function sin launchpad, bonding curve etc

--- src/interfaces/ILivoBondingCurve.sol
+++ src/interfaces/ILivoBondingCurve.sol
@@ -4,6 +4,8 @@ pragma solidity 0.8.28;
 interface ILivoBondingCurve {
     // thrown if trying to buy above graduation threshold + margin
     error MaxEthReservesExceeded();
+    // thrown if requesting more tokens/eth than the curve can provide
+    error InsufficientLiquidity();
 
     struct GraduationConfig {
         uint256 ethGraduationThreshold;
@@ -21,6 +23,18 @@ interface ILivoBondingCurve {
 
     /// @notice how much ETH will be received when selling an exact amount of tokens
     function sellExactTokens(uint256 ethReserves, uint256 tokenAmount) external view returns (uint256 ethReceived);
+
+    /// @notice How much ETH is required to buy an exact amount of tokens
+    function buyExactTokens(uint256 ethReserves, uint256 tokenAmount)
+        external
+        view
+        returns (uint256 ethRequired, bool canGraduate);
+
+    /// @notice How many tokens must be sold to receive an exact amount of ETH
+    function sellTokensForExactEth(uint256 ethReserves, uint256 ethAmount)
+        external
+        view
+        returns (uint256 tokensRequired);
 
     /// @notice When this eth reserves are matched, the token can graduate
     function ethGraduationThreshold() external view returns (uint256);
