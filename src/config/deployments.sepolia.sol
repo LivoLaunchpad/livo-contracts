0b8cbb89b22a1e1ad6df39112f6cc609458cc46e
jacopod <jacolansac@gmail.com>
2026-05-03 23:40:35 +0200

    config(sepolia): univ2 new graduator and factories

--- src/config/deployments.sepolia.sol
+++ src/config/deployments.sepolia.sol
@@ -14,7 +14,7 @@ library DeploymentsSepolia {
     // --- Core ---
     address internal constant LAUNCHPAD = 0xd9f8bbe437a3423b725c6616C1B543775ecf1110;
     address internal constant BONDING_CURVE = 0x1A7f2E2e4bdB14Dd75b6ce60ce7a6Ff7E0a3F3A5;
-    address internal constant GRADUATOR_UNIV2 = 0x9ac078c4E22917db450624632eA1997aD2ED4C73;
+    address internal constant GRADUATOR_UNIV2 = 0x67C95f98fa373781bc09efBff3c6a6E3614FE6e6;
     address internal constant GRADUATOR_UNIV4 = 0x85fE2051413a4b80b904f05841d1142FeF7f789c;
     address internal constant FEE_HANDLER = 0xC8e37Ff6bE0f3Ad39cF7481f8D5Ec89c96Bc48EF;
     address internal constant FEE_SPLITTER_IMPL = 0xDEAA2606f3F6Ff3B4277a30B7dCD382F9BA4bdB7;
@@ -31,11 +31,11 @@ library DeploymentsSepolia {
     address internal constant TAXABLE_TOKEN_SNIPER_PROTECTED_IMPL = 0x90191ADfC2DfB4E49897C55fD797aa97f5710cD9;
 
     // --- Factories ---
-    address internal constant FACTORY_UNIV2 = 0x933b7b99974bcf598c1E9abe8e513F5E08eEf11E;
+    address internal constant FACTORY_UNIV2 = 0x8a6f7A98Bb71d21D50cB799F7574A528b41Dc52b;
     address internal constant FACTORY_UNIV4 = 0x9039E9d741cF71C522ec3407dA34FA8eccE95531;
     address internal constant FACTORY_TAX_TOKEN = 0x09af239e3FfaD62482504868c335d9e0F2eCD3be;
 
-    address internal constant FACTORY_UNIV2_SNIPER_PROTECTED = 0x30Cb195a8b7E69636Dd5b0F99939deB6FFD5F1e1;
+    address internal constant FACTORY_UNIV2_SNIPER_PROTECTED = 0x0b428B51B81AD8BE825B55A8094eDbf7a4A55988;
     address internal constant FACTORY_UNIV4_SNIPER_PROTECTED = 0xFb81a889D90A8aB35D09fC4467174B0647B65563;
     address internal constant FACTORY_TAX_TOKEN_SNIPER_PROTECTED = 0xB975497b333B64352aAc32c1431C433ea9607719;
 
